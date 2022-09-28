c9928579f740a0a050f27e6bddc6c45962d58bbb
Andrew Arrow <oneone@gmail.com>
2022-09-28 11:04:26 -0700

    simple token highlights

--- syntax/highlight.go
+++ syntax/highlight.go
@@ -1,5 +1,20 @@
 package syntax
 
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
 func Highlight(s string) string {
-	return s
+	tokens := strings.Split(s, " ")
+	buff := []string{}
+	for _, t := range tokens {
+		color := "white"
+		if rand.Intn(2) == 0 {
+			color = "green"
+		}
+		buff = append(buff, fmt.Sprintf("[%s](fg:%s)", t, color))
+	}
+	return strings.Join(buff, " ")
 }
