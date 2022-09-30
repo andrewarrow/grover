b20e7be438b3b1798508b4e7a5f67081faa50d81
Andrew Arrow <oneone@gmail.com>
2022-09-30 06:16:43 -0700

    good failing test

--- screen/path_test.go
+++ screen/path_test.go
@@ -7,6 +7,22 @@ import (
 
 func TestLoadExisting(t *testing.T) {
 	files.Mkdir("data")
-	LoadPaths("main.go")
+
+	list := LoadPaths("main.go")
+	if len(list) != 1 {
+		t.Fatal("wrong length")
+	}
+	WritePaths(list)
+
+	list = LoadPaths("main2.go")
+	if len(list) != 2 {
+		t.Fatal("wrong length")
+	}
+	if list[0].Fullpath != "main2.go" {
+		t.Fatal("wrong order")
+	}
+
+	WritePaths(list)
+
 	files.Rmdir("data")
 }
