111a012397a45ce3694ef2a011580ec58a1204fb
Andrew Arrow <oneone@gmail.com>
2022-10-02 05:14:41 -0700

    progress with syntax not working yet

--- syntax/highlight.go
+++ syntax/highlight.go
@@ -6,16 +6,46 @@ import (
 	"strings"
 )
 
+type Token struct {
+	Text  string
+	Color string
+}
+
 func Highlight(s string) string {
 	replaced := strings.Replace(s, "\t", "  ", -1)
 	tokens := strings.Split(replaced, " ")
 	buff := []string{}
 	for _, t := range tokens {
-		color := "white"
-		if rand.Intn(2) == 0 {
-			color = "green"
+		for _, tokenWithColor := range evalToken(t) {
+			buff = append(buff, fmt.Sprintf("[%s](fg:%s)", tokenWithColor.Text,
+				tokenWithColor.Color))
 		}
-		buff = append(buff, fmt.Sprintf("[%s](fg:%s)", t, color))
 	}
 	return strings.Join(buff, " ")
+}
+
+func evalToken(t string) []*Token {
+	tokens := []*Token{}
+	color := "white"
+	if rand.Intn(2) == 0 {
+		color = "green"
+	}
+	open := strings.Split(t, "(")
+	closed := strings.Split(t, ")")
+	newToken := &Token{}
+
+	if len(open) > 0 && len(closed) == 0 {
+	} else if len(closed) > 0 && len(open) == 0 {
+	} else if len(closed) > 0 && len(open) > 0 {
+	} else {
+		newToken.Text = t
+		newToken.Color = color
+		tokens = append(tokens, newToken)
+	}
+
+	// t = func
+	// t = foo(bar
+	// t = string)
+	// t = foo()
+	return tokens
 }
