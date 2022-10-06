019d997a8ac72c79550e95f588ac4d916f2147a6
Andrew Arrow <oneone@gmail.com>
2022-10-06 05:51:16 -0700

    showing the word after func

--- swift/parse.go
+++ swift/parse.go
@@ -3,6 +3,7 @@ package swift
 import (
 	"fmt"
 	"grover/code"
+	"strings"
 )
 
 type Item struct {
@@ -16,7 +17,29 @@ type Function struct {
 func Parse(file string) []Item {
 	items := []Item{}
 
-	lines := code.ReadFileRemoveNewlines(file)
-	fmt.Println(lines)
+	chars := code.ReadFileRemoveNewlines(file)
+	words := ProcessCharacters(chars)
+	for i, w := range words {
+		if w == "func" {
+			fmt.Println(words[i+1])
+		}
+	}
 	return items
+}
+
+func ProcessCharacters(characters []string) []string {
+	word := []string{}
+	lastWords := []string{}
+	for _, c := range characters {
+		if c == "{" {
+		} else if c == "}" {
+		} else if c == " " {
+			completeWord := strings.Join(word, "")
+			lastWords = append(lastWords, completeWord)
+			word = []string{}
+		} else {
+			word = append(word, c)
+		}
+	}
+	return lastWords
 }
