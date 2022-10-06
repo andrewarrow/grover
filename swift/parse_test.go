1eee1438d1e48c2cfa7ead080cc2ed9438408fd2
Andrew Arrow <oneone@gmail.com>
2022-10-06 16:43:20 -0700

    good failing test

--- swift/parse_test.go
+++ swift/parse_test.go
@@ -1,11 +1,21 @@
 package swift
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestParse(t *testing.T) {
 	item := Parse("example.swift")
-	fmt.Println(item)
+	if len(item.Classes) != 2 {
+		t.Fatal("wrong length")
+	}
+	name := item.Classes[0].Name
+	if name != "AnotherClass" {
+		t.Fatal("wrong name", name)
+	}
+	name = item.Classes[0].Name
+	if name != "RootViewController" {
+		t.Fatal("wrong name", name)
+	}
+
 }
